6202220654088eeca026b0903941a67e22067d8c
AlanYang <alanyang@AlanYangs-MacBook-Pro.local>
2016-06-21 15:55:02 +0800

    add zookeeper container for thrift node cluster

--- client.js
+++ client.js
@@ -1,66 +1,106 @@
 'use strict'
 const thrift = require('thrift')
+const zookeeper = require('node-zookeeper-client')
 const User = require('./idescription/gen-nodejs/UserService')
 const UserTypes = require('./idescription/gen-nodejs/user_types')
 const Tag = require('./idescription/gen-nodejs/TagService')
 const TagType = require('./idescription/gen-nodejs/tag_types')
 
 const transport = thrift.TBufferedTransport()
-const protocol  = thrift.TBinaryProtocol()
+const protocol = thrift.TBinaryProtocol()
 
-//connect to user server
-const userConn = thrift.createConnection('localhost', 9090, {transport, protocol})
-userConn.on('error', console.error)
-const userService = thrift.createClient(User, userConn)
 
-//connect to tag server
-const tagConn = thrift.createConnection('localhost', 9999, {transport, protocol})
-tagConn.on('error', console.error)
-const tagService = thrift.createClient(Tag, tagConn)
+const client = zookeeper.createClient('127.0.0.1:2181')
+client.connect()
+client.once('connected', () => {
+    client.getData('/userServer', event => {}, (err, data, stat) => {
+        const serviceAddr = data.toString().split(':')
+            //connect to user server
+        const userConn = thrift.createConnection(serviceAddr[0], parseInt(serviceAddr[1]), {
+            transport, protocol
+        })
+        userConn.on('error', console.error)
+        const userService = thrift.createClient(User, userConn)
 
-//call service getUser
-userService.getUser('popqweuiqwie13123').then(user => {
-    console.log('Got a user')
-    console.log(user)
-    console.log('======================')
-})
+        //connect to tag server
+        // const tagConn = thrift.createConnection('localhost', 9999, {
+        //     transport, protocol
+        // })
+        // tagConn.on('error', console.error)
+        // const tagService = thrift.createClient(Tag, tagConn)
 
-//call resetPassword
-userService.resetPassword('popqweuiqwie13123', 'oioqwe').then(ret => {
-    console.log('reset password')
-    console.log(err || ret)
-    console.log('======================')
-})
-
-//call updateUser need generate a User struct, this is update user's status
-const user = new UserTypes.User
-user.status = UserTypes.Status.unactivated
-userService.updateUser('popqweuiqwie13123', user).then(ret => {
-    console.log('update user')
-    console.log(err || ret)
-    console.log('======================')
-})
-
-userService.isAuthenticated('123123asdads').then(ret => {
-    console.log(ret && 'Authenticated' || 'Unauthenticated')
-    console.log('======================')
-})
-
-const right = 'edit'
-const id = 'popqweuiqwie13123'
-userService.getUser(id, (err, user) => {
-    userService.hasRight(id, right, (err, ret) => {
-        console.log(ret && `${user.email} has ${right} right` || `reject ${right} right`)
-        if(ret) {
-            //has edit right
-            tagService.saveTag(212.23, 78.12, (new Date).getTime(), 'my tag', '123askdjkh', (err, ret) => {
-                console.log('has right, edit tag begin')
-                console.log(ret && 'save tag done')
-                console.log('======================') 
+        //call service getUser
+        userService.getUser('popqweuiqwie13123')
+            .then(user => {
+                console.log('Got a user')
+                console.log(user)
+                console.log('======================')
             })
-        }
-        console.log('======================')
+
+        //call resetPassword
+        userService.resetPassword('popqweuiqwie13123', 'oioqwe')
+            .then(ret => {
+                console.log('reset password')
+                console.log(err || ret)
+                console.log('======================')
+            })
+
+        //call updateUser need generate a User struct, this is update user's status
+        const user = new UserTypes.User
+        user.status = UserTypes.Status.unactivated
+        userService.updateUser('popqweuiqwie13123', user)
+            .then(ret => {
+                console.log('update user')
+                console.log(err || ret)
+                console.log('======================')
+            })
+
+        userService.isAuthenticated('123123asdads')
+            .then(ret => {
+                console.log(ret && 'Authenticated' || 'Unauthenticated')
+                console.log('======================')
+            })
+
+        const right = 'edit'
+        const id = 'popqweuiqwie13123'
+        userService.getUser(id, (err, user) => {
+            userService.hasRight(id, right, (err, ret) => {
+                console.log(ret && `${user.email} has ${right} right` || `reject ${right} right`)
+                if (ret) {
+                    //has edit right
+                    // tagService.saveTag(212.23, 78.12, (new Date)
+                    //     .getTime(), 'my tag', '123askdjkh', (err, ret) => {
+                    //         console.log('has right, edit tag begin')
+                    //         console.log(ret && 'save tag done')
+                    //         console.log('======================')
+                    //     })
+                }
+                console.log('======================')
+            })
+        })
     })
 })
+
+// const zk = new Zookeeper({
+//     connect: '127.0.0.1:2181',
+//     timeout: 12000,
+//     debug_level: Zookeeper.ZOO_LOG_LEVEL_WARN,
+//     host_order_deterministic: false,
+//     data_as_buffer: false
+// })
+
+// zk.connect(err => {
+//     if(err) {
+//         console.error(err)
+//         process.exit(2)
+//     }
+// })
+
+// zk.on(Zookeeper.on_connected, client => {
+//     console.log(client)
+//     client.a_get('/userServer', true, (r, err, children) => {
+//         console.log(r, err, children)
+//     })
+// })
 
 //===============================
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict'
 
 const thrift = require('thrift')
+const zookeeper = require('node-zookeeper-client')
 const User = require('./idescription/gen-nodejs/UserService')
 const UserType = require('./idescription/gen-nodejs/user_types')
 const Tag = require('./idescription/gen-nodejs/TagService')
 const TagType = require('./idescription/gen-nodejs/tag_types')
+    // const service = require('./service')
 
 const data = [{
-	_id: 'hjahsdjahdjahjsdjll123',
+    _id: 'hjahsdjahdjahjsdjll123',
     email: 'ljqwe2@qq.com',
     role: 'user',
     hashedPassword: 'askhd123123',
@@ -21,7 +23,7 @@ const data = [{
     location: 98621,
     password: 'wwwasd123' //plain text
 }, {
-	_id: 'popqweuiqwie13123',
+    _id: 'popqweuiqwie13123',
     email: 'miracle@gmail.com',
     role: 'admin',
     hashedPassword: 'fuckteamog',
@@ -37,46 +39,46 @@ const data = [{
 }]
 
 const errorsUnmatchOldPassword = 1
-const errorUserUnexsiting      = 2
+const errorUserUnexsiting = 2
 
 //chang for mongodb find
 const getUser = (id) => {
-	for(let u of data) {
-		if(u._id === id) {
-			return JSON.parse(JSON.stringify(u))
-		}
-	}
+    for (let u of data) {
+        if (u._id === id) {
+            return JSON.parse(JSON.stringify(u))
+        }
+    }
 }
 
 const userServer = thrift.createServer(User, {
-  //   getUser(id, result) {
-  //   	//replace to mongo findone
-  //   	let _user = getUser(id)
-		// //suppose io time mongodb find use time 100ms
-		// let user = new UserType.User
-		// result(null, Object.assign(user, _user))
-  //   },
-	
-	//server promise
+    //   getUser(id, result) {
+    //   	//replace to mongo findone
+    //   	let _user = getUser(id)
+    // //suppose io time mongodb find use time 100ms
+    // let user = new UserType.User
+    // result(null, Object.assign(user, _user))
+    //   },
+
+    //server promise
     getUser(id) {
-		let _user = getUser(id)
-		let user  = new UserType.User
-		return Object.assign(user, _user)
-    },	
+        let _user = getUser(id)
+        let user = new UserType.User
+        return Object.assign(user, _user)
+    },
 
     resetPassword(id, old, result) {
-		const _user = getUser(id)
-		setTimeout(()=>{
-			if(!_user || _user.password != old) {
-				const error = new UserType.UserException
-				error.errorCode = errorsUnmatchOldPassword
-				error.reason = 'invalid user or unmatch old password'
-				result(error, false)
-			} else {
-				//update user password in db
-				result(null, true)
-			}
-		}, 189)
+        const _user = getUser(id)
+        setTimeout(() => {
+            if (!_user || _user.password != old) {
+                const error = new UserType.UserException
+                error.errorCode = errorsUnmatchOldPassword
+                error.reason = 'invalid user or unmatch old password'
+                result(error, false)
+            } else {
+                //update user password in db
+                result(null, true)
+            }
+        }, 189)
     },
 
     sendResetPasswordMail(email, result) {
@@ -84,44 +86,85 @@ const userServer = thrift.createServer(User, {
     },
 
     updateUser(id, data, result) {
-		const _user = getUser(id)
-		setTimeout(()=>{
-			if(!_user) {
-				const error = new UserType.UserException
-				error.errorCode = errorUserUnexsiting
-				error.reason = 'User unexsiting'
-				result(error, false)	
-			} else {
-				Object.assign(_user, data)
-				//update db for _user
-				result(null, true)
-			}
-		})
+        const _user = getUser(id)
+        setTimeout(() => {
+            if (!_user) {
+                const error = new UserType.UserException
+                error.errorCode = errorUserUnexsiting
+                error.reason = 'User unexsiting'
+                result(error, false)
+            } else {
+                Object.assign(_user, data)
+                //update db for _user
+                result(null, true)
+            }
+        })
     },
 
     registerUser(user, result) {
-		
+
     },
 
     activateUser(id, result) {
-		result(null, true)
+        result(null, true)
     },
 
-	isAuthenticated(token, result) {
-		result(null, true)
-	},
+    isAuthenticated(token, result) {
+        result(null, true)
+    },
 
-	hasRight(id, right, result) {
-		result(null, !!(parseInt(Math.random()*100)%2))
-	}
+    hasRight(id, right, result) {
+        result(null, !!(parseInt(Math.random() * 100) % 2))
+    }
 })
 
 userServer.listen(9090)
 
-const tagServer = thrift.createServer(Tag, {
-	saveTag(x, y, time, title, vid, result) {
-		result(null, true)
-	}
+const client = zookeeper.createClient('127.0.0.1:2181')
+client.once('connected', () => {
+	client.create('/userServer', new Buffer('127.0.0.1:9090'),  err => {
+		if(err) {
+			console.error(err)
+		} else {
+			console.log('Create /userServer success')
+		}
+		client.close()
+	})
 })
+client.connect()
 
-tagServer.listen(9999)
+// const tagServer = thrift.createServer(Tag, {
+//     saveTag(x, y, time, title, vid, result) {
+//         result(null, true)
+//     }
+// })
+
+// tagServer.listen(9999)
+
+
+// register tag server to zookeeper
+// const zkClient = new Zookeeper({
+//     connect: '127.0.0.1:2181',
+//     timeout: 12000,
+//     debug_level: Zookeeper.ZOO_LOG_LEVEL_WARN,
+//     host_order_deterministic: false,
+//     data_as_buffer: false
+// })
+
+// zkClient.on(Zookeeper.on_connected, client => {
+//     client.a_create('/userServer', '', Zookeeper.ZOO_EPHEMERAL,   (r, err, path) => {
+//         if (r != 0) {
+//             console.error(`Create node /userServer error ${err}`)
+//             throw err
+//         } else {
+//             console.log('Create /userServer success')
+//         }
+//     })
+// })
+
+// zkClient.connect(err => {
+//     if (err) {
+//         console.error(err)
+//         throw err
+//     }
+// })
